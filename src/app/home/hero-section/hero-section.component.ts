83a67393c6638936eb53753771cd0c8b30919ea1
Lara <lara@laranewsom.com>
2023-04-30 10:21:51 -0500

    cleaned up unused code

--- src/app/home/hero-section/hero-section.component.ts
+++ src/app/home/hero-section/hero-section.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero-section',
   templateUrl: './hero-section.component.html',
-  styleUrls: ['./hero-section.component.css']
 })
 export class HeroSectionComponent {
   images = [
