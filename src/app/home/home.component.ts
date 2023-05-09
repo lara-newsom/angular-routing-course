8637e8df73ad24206ed79b22676414244006ed5b
Lara <lara@laranewsom.com>
2023-05-08 22:14:36 -0500

    logging lifecycle hooks

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor() {
+    console.log('constructor Home Component');
+  }
+  ngOnInit(): void {
+      console.log('init Home Component');
+  }
 
 }
