bcfc078a0f72b1bfe58331888cd7d6faec9e8a90
Lara <lara@laranewsom.com>
2023-06-18 21:53:08 -0500

    added router tracing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withDebugTracing } from '@angular/router';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
+    provideRouter(
+      routes,
+      withDebugTracing(),
+    ),
   ]
 };
 
