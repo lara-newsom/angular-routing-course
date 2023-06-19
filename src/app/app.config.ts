d9e66ca440272833e66150999a88d8543f807d91
Lara <lara@laranewsom.com>
2023-06-18 20:01:56 -0500

    enable routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { Route, provideRouter } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const ROUTES: Route[] = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 export const CONFIG = {
   providers: [
     provideAnimations(),
+    provideRouter(ROUTES)
   ],
 };
 
