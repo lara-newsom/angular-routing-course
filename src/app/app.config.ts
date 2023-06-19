c8bca91434720bd29b2d1da674cc2544d51808b2
Lara <lara@laranewsom.com>
2023-06-18 20:22:00 -0500

    renaming to Angular default

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,28 +1,10 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { Route, provideRouter } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { ApplicationConfig } from '@angular/core';
 
-const ROUTES: Route[] = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: '**',
-    component: NotFoundComponent,
-  },
-];
-
-export const CONFIG = {
+export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
-    provideRouter(ROUTES)
-  ],
+  ]
 };
+
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { CONFIG } from './app/app.config';
+import { appConfig } from './app/app.config';
 
-  bootstrapApplication(AppComponent, CONFIG);
+  bootstrapApplication(AppComponent, appConfig);
