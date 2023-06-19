649c7f9c4b46d72a09c831931b55d4d958ac479c
Lara <lara@laranewsom.com>
2023-06-18 20:33:46 -0500

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-  
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
