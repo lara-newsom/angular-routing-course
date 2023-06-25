17234ed94ac47916ba75c2ecf743b2fc588a1ca2
Lara <lara@laranewsom.com>
2023-06-25 14:25:01 -0500

    deleted unneeded code

--- src/app/home/hero-section/hero-section.component.ts
+++ src/app/home/hero-section/hero-section.component.ts
@@ -5,16 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './hero-section.component.html',
 })
 export class HeroSectionComponent {
-  images = [
-    '../../assets/images/hero-1.png',
-    '../../assets/images/hero-2.png',
-    '../../assets/images/hero-3.png',
-  ];
-
-  imageIndex = 0;
-
-  changeImage(index: number) {
-    this.imageIndex = index;
-  }
-
+  image = '../../assets/images/hero-3.png';
 }
