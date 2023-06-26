fa5c21a0c8a2fa21a9e0c495fcd99c10733fd0bb
Lara <lara@laranewsom.com>
2023-06-25 20:30:22 -0500

    in progress changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,38 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductsViewComponent } from './products-view/products-view.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+export enum ROUTER_TOKENS {
+  HOME = 'home',
+  SHOP = 'shop',
+  CONTACT = 'contact',
+  ABOUT = 'about',
+}
 
 export const ROUTES: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: ROUTER_TOKENS.HOME,
     pathMatch: 'full',
   },
   {
-    path: 'home',
+    path: ROUTER_TOKENS.HOME,
     component: HomeComponent,
+  },
+  {
+    path: ROUTER_TOKENS.SHOP,
+    component: ProductsViewComponent,
+  },
+  {
+    path: ROUTER_TOKENS.CONTACT,
+    component: ContactComponent,
+  },
+  {
+    path: ROUTER_TOKENS.ABOUT,
+    component: AboutComponent,
   },
   {
     path: '**',
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Category } from '../models/pie';
 import { PieService } from '../services/pie.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { ROUTER_TOKENS } from '../app.routes';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   imports: [
     MatMenuModule,
     MatButtonModule,
+    RouterLink,
   ],
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -16,9 +19,9 @@ import { MatMenuModule } from '@angular/material/menu';
 })
 export class HeaderComponent {
   readonly Category = Category;
-  constructor(
-    private readonly pieService: PieService
-  ){}
+  readonly ROUTER_TOKENS = ROUTER_TOKENS;
+
+  private readonly pieService = inject(PieService);
 
   changeCategory(category: Category){
     this.pieService.setSelectedCategory(category);
