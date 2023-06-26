8b9fb5aad61b1a7d685766ee9beda8aeccbaf186
Lara <lara@laranewsom.com>
2023-06-25 19:52:50 -0500

    added module four changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -11,6 +12,7 @@ import { HomeComponent } from './home/home.component';
     MatButtonModule,
     HeaderComponent,
     HomeComponent,
+    RouterOutlet,
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withDebugTracing } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
+    provideRouter(ROUTES,
+      withDebugTracing(),
+    ),
   ]
 };
 
