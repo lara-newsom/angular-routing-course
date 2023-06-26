0aef25667741ceb40f5ab4ba4526b9726f6c34a2
Lara <lara@laranewsom.com>
2023-06-25 20:15:50 -0500

    removed router link

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,14 +3,12 @@ import { Category } from '../models/pie';
 import { PieService } from '../services/pie.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
-import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   imports: [
     MatMenuModule,
     MatButtonModule,
-    RouterLink
   ],
   selector: 'app-header',
   templateUrl: './header.component.html',
--- src/app/home/sub-section/card/card.component.ts
+++ src/app/home/sub-section/card/card.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
 import { Pie } from 'src/app/models/pie';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -8,7 +7,6 @@ import { CartService } from 'src/app/services/cart.service';
   standalone: true,
   imports: [
     MatButtonModule,
-    RouterLink,
   ],
   selector: 'app-card',
   templateUrl: './card.component.html',
