a6d07674f4a98a8d81fbdfd46fb64a5a87995560
Lara <lara@laranewsom.com>
2023-06-25 19:49:42 -0500

    resetting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
   imports: [
-    RouterOutlet,
     MatMenuModule,
     MatButtonModule,
     HeaderComponent,
