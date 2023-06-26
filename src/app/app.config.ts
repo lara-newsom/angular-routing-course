a02c40f3a5327d4a493f3235c92db9082ab2d4b3
Lara <lara@laranewsom.com>
2023-06-25 19:56:09 -0500

    resetting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withDebugTracing } from '@angular/router';
-import { routes } from './app.routes';
+import { ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideRouter(
-      routes,
+      ROUTES,
       withDebugTracing(),
     ),
   ]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [
+export const ROUTES: Routes = [
   {
     path: '',
     redirectTo: 'home',
