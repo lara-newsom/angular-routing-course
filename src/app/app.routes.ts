1e00b8709c03d597d38cccb4f7133f4931af297b
Lara <lara@laranewsom.com>
2023-06-30 21:02:03 -0500

    routing changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { CartAuthGuard } from './cart-auth-route-guard';
+import { HomeUpdatedComponent } from './home-updated/home-updated.component';
+import { inject } from '@angular/core';
+import { FeatureFlagService } from './services/feature-flag.service';
+import { map } from 'rxjs';
+import { AuthService } from './services/auth.service';
+import { HelloService } from './services/hello.service';
 
 export enum ROUTER_TOKENS {
   HOME = 'home',
@@ -22,6 +27,16 @@ export const ROUTES: Routes = [
   },
   {
     path: ROUTER_TOKENS.HOME,
+    component: HomeUpdatedComponent,
+    canMatch: [() => {
+      const flagService = inject(FeatureFlagService);
+      return flagService.featureFlags.pipe(map((flag) => {
+        return !!flag.home
+      }))
+    }]
+  },
+  {
+    path: ROUTER_TOKENS.HOME,
     component: HomeComponent,
   },
   {
@@ -30,7 +45,30 @@ export const ROUTES: Routes = [
   },
   {
     path: ROUTER_TOKENS.CONTACT,
-    loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent)
+    loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent),
+    canActivate: [
+      () => {
+        const flagService = inject(FeatureFlagService);
+        return flagService.featureFlags.pipe(map((flag) => !!flag.contact))
+      }],
+    canMatch: [
+      () => {
+        const authService = inject(AuthService);
+        return authService.userAuth.pipe(map((permissions) => !!permissions?.includes('contact')))
+      }],
+    resolve: {userHello: () => {
+      const helloService = inject(HelloService);
+
+      return helloService.getUserHello()
+    }}
+  },
+  {
+    path: ROUTER_TOKENS.CONTACT,
+    redirectTo: ROUTER_TOKENS.NOT_AUTH,
+  },
+  {
+    path: ROUTER_TOKENS.NOT_AUTH,
+    loadComponent: () => import('./not-authorized/not-authorized.component').then(m => m.NotAuthorizedComponent),
   },
   {
     path: ROUTER_TOKENS.ABOUT,
@@ -40,7 +78,12 @@ export const ROUTES: Routes = [
     path: ROUTER_TOKENS.CHECKOUT,
     outlet: ROUTER_TOKENS.CART,
     loadComponent: () => import('./cart/cart.component').then(m => m.CartComponent),
-    canActivate: [CartAuthGuard]
+    canActivate: [
+      () => {
+        const authService = inject(AuthService);
+        return authService.userAuth.pipe(map((permissions) => !!permissions?.includes('cart')))
+      }
+    ]
   },
   {
     path: '**',
