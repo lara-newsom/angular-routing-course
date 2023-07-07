2221d2667199b8a82dd062c2415f24bbb2441913
Lara <lara@laranewsom.com>
2023-07-06 23:04:29 -0500

    added cart test setup

--- src/app/cart/cart.component.spec.ts
+++ src/app/cart/cart.component.spec.ts
@@ -1,21 +1,56 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CartComponent } from './cart.component';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { createSpyFromClass, provideAutoSpy } from 'jest-auto-spies';
+import { Router } from '@angular/router';
+import { CartService } from '../services/cart.service';
+import { fireEvent, render, screen } from '@testing-library/angular';
+import { ROUTER_TOKENS } from '../app-route.constants';
+import { signal } from '@angular/core';
 
 describe('CartComponent', () => {
-  let component: CartComponent;
-  let fixture: ComponentFixture<CartComponent>;
+  const setup = async() => {
+    const mockCartService = {
+      cartItemsPlusQuantity: signal([]),
+      subtotal: signal(1),
+      salesTax: signal(1),
+      total: signal(1),
+    };
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [CartComponent]
-    });
-    fixture = TestBed.createComponent(CartComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    // mock router service and spy on navigate
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    const options = {
+      imports: [
+        CommonModule,
+        CurrencyPipe,
+        MatButtonModule,
+        FormsModule,
+        MatProgressSpinnerModule,
+      ],
+      providers: [
+        provideAutoSpy(CartService),
+        {
+          provide: CartService,
+          useValue: mockCartService,
+        },
+        // provide mock router
+      ]
+    };
+
+    const { fixture } = await render(CartComponent, options);
+
+    return {
+      fixture,
+    };
+  }
+
+  it('calls router.navigate on close', async() => {
+    // setup test
+
+    // find and click close button
+
+    // expect router.navigate to have been called
   });
 });
