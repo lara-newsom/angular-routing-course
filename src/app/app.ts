bdde790162762f2fda8b1df4f56cfd456ba2edb2
Lara Newsom <laramnewsom@gmail.com>
2025-07-31 00:07:36 -0500

    added about page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ComboCardViewComponent } from './combo-card-view/combo-card-view.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
     selector: 'app-root',
