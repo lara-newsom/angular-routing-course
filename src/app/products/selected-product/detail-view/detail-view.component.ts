f18758979b048467e9954e401fd12ebb681770ad
Lara Newsom <laramnewsom@gmail.com>
2025-08-03 16:39:07 -0500

    removed unused import

--- src/app/products/selected-product/detail-view/detail-view.component.ts
+++ src/app/products/selected-product/detail-view/detail-view.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { PieService } from '../../../services/pie.service';
-import { MatButton } from '@angular/material/button';
 import { CartService } from 'src/app/services/cart.service';
 import { CartButtonComponent } from 'src/app/shared-ui/cart-button/cart-button.component';
 
@@ -8,7 +7,7 @@ import { CartButtonComponent } from 'src/app/shared-ui/cart-button/cart-button.c
     selector: 'app-detail-view',
     templateUrl: './detail-view.component.html',
     styleUrls: ['./detail-view.component.scss'],
-    imports: [MatButton, CartButtonComponent],
+    imports: [CartButtonComponent],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DetailViewComponent {
