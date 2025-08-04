1b4c9c89cf92af912d2f3d49c8b04b4b4d78017a
Lara Newsom <laramnewsom@gmail.com>
2025-08-04 00:28:28 -0500

    cleaned up app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from "./products/products.component";
-import { ContactComponent } from "./contact/contact.component";
-import { NotFoundComponent } from "./not-found/not-found.component";
-import { CartComponent } from "./cart/cart.component";
-import { AboutComponent } from "./about/about.component";
-import { LoginComponent } from "./login/login.component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.html',
     styleUrls: ['./app.scss'],
-    imports: [HeaderComponent, HomeComponent, LoginComponent],
+    imports: [HeaderComponent, HomeComponent],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
