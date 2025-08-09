5ae26c5584cdbfc4d95a2fcf7a10e2a90a5dcfbc
Lara Newsom <laramnewsom@gmail.com>
2025-08-09 18:16:58 -0500

    completed code changes for M2_V5_E

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { ContactComponent } from './contact/contact.component';
 import { CartComponent } from './cart/cart.component';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -40,5 +41,9 @@ export const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
