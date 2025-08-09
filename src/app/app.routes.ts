e6ba31ecddf5f8c14481965902273f4a2a81a133
Lara Newsom <laramnewsom@gmail.com>
2025-08-09 17:49:47 -0500

    completed code changes for M2_V4_E

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,11 @@ import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     component: HomeComponent,
   },
