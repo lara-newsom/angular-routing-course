db42b13a7ac46a5fbaa8ca5a6becf03ec72058e3
Lara Newsom <laramnewsom@gmail.com>
2025-08-09 18:29:25 -0500

    completed code changes for M2_V6_E

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
+  router = inject(Router);
+  constructor() {
+    console.log(this.router)
+  }
 }
