1b388c5c8411d966f7a56b48d1995846c1b2081a
Lara Newsom <laramnewsom@gmail.com>
2025-08-09 23:08:34 -0500

    setup for demo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,43 +8,51 @@ import { CartComponent } from './cart/cart.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
+export const HOME_ROUTE = 'home';
+export const PRODUCTS_ROUTE = 'products';
+export const DETAIL_ROUTE = 'detail';
+export const LOGIN_ROUTE = 'login';
+export const CONTACT_ROUTE = 'contact';
+export const CART_ROUTE = 'cart';
+export const ABOUT_ROUTE = 'about';
+
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: HOME_ROUTE,
     pathMatch: 'full',
   },
   {
-    path: 'home',
+    path: HOME_ROUTE,
     component: HomeComponent,
   },
   {
-    path: 'products',
+    path: PRODUCTS_ROUTE,
     children: [
       {
         path: '',
         component: AllProductsComponent,
       },
       {
-        path: 'detail',
+        path: DETAIL_ROUTE,
         component: ProductDetailComponent,
       },
     ]
   },
   {
-    path: 'login',
+    path: LOGIN_ROUTE,
     component: LoginComponent,
   },
   {
-    path: 'contact',
+    path: CONTACT_ROUTE,
     component: ContactComponent,
   },
   {
-    path: 'cart',
+    path: CART_ROUTE,
     component: CartComponent,
   },
   {
-    path: 'about',
+    path: ABOUT_ROUTE,
     component: AboutComponent,
   },
   {
