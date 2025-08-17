37e7b5ba13dcaa230c75b5d4824f35e8fd5fb1d8
Lara Newsom <laramnewsom@gmail.com>
2025-08-17 16:37:10 -0500

    Breadcrumbs routerlinks

--- src/app/shared-ui/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared-ui/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,21 @@
 import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { PieService } from '../../services/pie.service';
 import { MatButton } from '@angular/material/button';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { DETAIL_ROUTE, HOME_ROUTE, PRODUCTS_ROUTE } from 'src/app/app.routes';
 
 @Component({
     selector: 'app-breadcrumbs',
     templateUrl: './breadcrumbs.component.html',
     styleUrls: ['./breadcrumbs.component.scss'],
-    imports: [MatButton],
+    imports: [MatButton, RouterLink, RouterLinkActive],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BreadcrumbsComponent {
   readonly route = input.required<string>();
   protected readonly pieService = inject(PieService);
+
+    protected readonly PRODUCTS_ROUTE = PRODUCTS_ROUTE;
+    protected readonly HOME_ROUTE = HOME_ROUTE;
+    protected readonly DETAIL_ROUTE = DETAIL_ROUTE;
 }
