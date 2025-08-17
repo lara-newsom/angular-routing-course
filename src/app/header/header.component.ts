523e9742fa4f640f14b9551352921d3e52452cb5
Lara Newsom <laramnewsom@gmail.com>
2025-08-17 11:51:13 -0500

    completed code for M3_V2E

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,17 +5,35 @@ import { MatMenuItem, MatMenuTrigger, MatMenu } from '@angular/material/menu';
 import { MatButton } from '@angular/material/button';
 import { CartService } from '../services/cart.service';
 import { AuthService } from '../services/auth.service';
+import {
+  ABOUT_ROUTE,
+  HOME_ROUTE,
+  LOGIN_ROUTE,
+  PRODUCTS_ROUTE,
+  DETAIL_ROUTE,
+  CONTACT_ROUTE,
+  CART_ROUTE,
+} from '../app.routes';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
-    imports: [MatMenuItem, MatButton, MatMenuTrigger, MatMenu]
+    imports: [MatMenuItem, MatButton, MatMenuTrigger, MatMenu, RouterLink]
 })
 export class HeaderComponent {
   protected readonly pieService = inject(PieService);
   protected readonly authService = inject(AuthService);
   protected cartService = inject(CartService);
+
+  protected readonly PRODUCTS_ROUTE = PRODUCTS_ROUTE;
+  protected readonly HOME_ROUTE = HOME_ROUTE;
+  protected readonly LOGIN_ROUTE = LOGIN_ROUTE;
+  protected readonly ABOUT_ROUTE = ABOUT_ROUTE;
+  protected readonly DETAIL_ROUTE = DETAIL_ROUTE;
+  protected readonly CONTACT_ROUTE = CONTACT_ROUTE;
+  protected readonly CART_ROUTE = CART_ROUTE;
 
   changeCategory(category: Category){
     this.pieService.clearSelectedPie();
--- src/app/products/all-products/all-products.component.ts
+++ src/app/products/all-products/all-products.component.ts
@@ -1,17 +1,21 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { PieService } from '../../services/pie.service';
 import { BreadcrumbsComponent } from '../../shared-ui/breadcrumbs/breadcrumbs.component';
+import { RouterLink } from '@angular/router';
+import { DETAIL_ROUTE } from '../../app.routes';
 
 @Component({
   selector: 'app-all-products',
   templateUrl: './all-products.component.html',
   styleUrl: './all-products.component.scss',
-  imports: [BreadcrumbsComponent],
+  imports: [BreadcrumbsComponent, RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AllProductsComponent {
   readonly pieService = inject(PieService);
   pies = this.pieService.filteredPies;
+
+  protected readonly DETAIL_ROUTE = DETAIL_ROUTE;
 
   selectPie(id: string){
     this.pieService.setSelectedPie(id);
