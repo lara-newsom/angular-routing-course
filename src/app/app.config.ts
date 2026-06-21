1b5c094288b93a2a0354035c96a594754de346d7
Lara Newsom <laramnewsom@gmail.com>
2026-06-21 12:21:12 -0500

    updated to V22

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withInMemoryScrolling, withPreloading } from '@angular/router';
 import {InMemoryWebApiModule }from 'angular-in-memory-web-api';
 
 import { routes } from './app.routes';
@@ -19,7 +19,8 @@ export const appConfig: ApplicationConfig = {
       withComponentInputBinding(),
       withInMemoryScrolling({
         scrollPositionRestoration: 'enabled'
-      })
+      }),
+      withPreloading(PreloadAllModules)
     ),
     provideHttpClient(),
     importProvidersFrom(
